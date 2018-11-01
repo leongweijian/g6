01eac8a259127cd3eb779ba33e14a3d3414bb926
TARUC <TARUC@PC-90>
2018-11-01 14:46:24 +0800

    completed story updated #989876

--- Documents/NetBeansProjects/JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ Documents/NetBeansProjects/JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -17,6 +17,7 @@ public class JavaApplication1 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.print("Hello");
+        System.out.print("Hello world");
         
     }
     
